e39fc9406ddf6483321c65730430c85726c9adf6
Abhi09jeet2000 <abhirbkulkarni@gmail.com>
2025-06-22 04:01:54 -0700

    connection work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Vapi from "@vapi-ai/web";
 const apiKey = "27093ede-e32e-4331-b91f-a58bd996d130";
 const assistantId = "c35c764f-8b48-4935-8129-0787d1caf481";
 const vapi = new Vapi(apiKey);
+const socket = new WebSocket("ws://localhost:8765");
 
 const Home = () => {
   const [statements, setStatements] = useState([]); // { sender: 'agent', text: string }
@@ -25,15 +26,44 @@ const Home = () => {
     // eslint-disable-next-line
   }, []);
 
+  socket.onopen = () => {
+    console.log("Connected to Python WebSocket");
+  };
+  
+  socket.onmessage = (event) => {
+    console.log("Agent response:", event.data);
+  };
+  
+  const sendToAgent = (text) => {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(text);
+    } else {
+      console.error("WebSocket not open.");
+    }
+  };
+  
+
   const handleRecord = () => {
     if (!isRecording) {
-      setIsRecording(true);
-      vapi.start(assistantId);
-      alert('Call started!');
+      try {
+        setIsRecording(true);
+        vapi.start(assistantId);
+        alert('Call started!');
+      } catch (error) {
+        console.error("An error occurred during start:", error);
+        alert("Failed to start call: " + error.message);
+        setIsRecording(false);
+      }
     } else {
-      setIsRecording(false);
-      vapi.stop(assistantId);
-      alert('Call stopped!');
+      try {
+        setIsRecording(false);
+        vapi.stop(assistantId);
+        sendToAgent("To summarize, you’ve been approved for a loan of 1200 at an interest rate of 6% for a term of 12 months. The funds will be deposited into your SmartLoan Wallet shortly. You can review your repayment schedule and manage your loan details through your dashboard. Thank you for choosing SmartLoan!")
+        alert('Call stopped!');
+      } catch (error) {
+        console.error("An error occurred during stop:", error);
+        alert("Failed to stop call: " + error.message);
+      }
     }
   };
 
